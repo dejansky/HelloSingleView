6716fef28aff6a1fee577df8f0a0c09fa88d81bc
Dejan Arsenijevic <dejanarsen@gmail.com>
2019-11-05 10:23:10 +0100

    Initial Commit

---
+++ HelloSingleView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloSingleView
+//
+//  Created by Dejan Arsenijevic on 2019-11-05.
+//  Copyright © 2019 Dejan Arsenijevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
